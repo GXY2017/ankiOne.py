a21048c23b7243b85651fae23e572c5fbcc3b901
GXY2017 <gxy493@gmail.com>
2021-06-16 23:37:23 +0800

    Extract words from text file for the first time.

--- ankiOne.py
+++ ankiOne.py
@@ -1,8 +1,30 @@
+import pandas as pd
+import numpy as np
+import os
 import requests
 from PyDictionary import PyDictionary as pydict
 import nltk
 # nltk.download()  # download to C:\..
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import WordNetLemmatizer
+from stardict import DictCsv
+from spellchecker import SpellChecker
+
+spell = SpellChecker(distance=1)
+
+
+def stripword(word):
+    """
+    去除整个字符串中非字母和数字的部分，并将字母转为小写
+    :param word:
+    :return:
+    """
+    return (''.join([n for n in word if n.isalnum()])).lower()
+
+
+stripword('be- tween')
+
+textUrl = r"C:\Users\gxy49\Downloads\Philosophers_Stone.txt"
 
 # 导入完整的英语词汇表
 words = set(nltk.corpus.words.words())
@@ -13,25 +35,50 @@ response = requests.get(english_most_common_10k)
 data = response.text
 set_of_common_words = {x for x in data.split('\n')}
 # 导入文章
-with open(r"C:\Users\XIYU.GONG\Downloads\Lehman-Brothers_-Modelling-Credit-Theory-and-Practice.txt",
-          "r", encoding='utf-8') as f:
-    text = f.read().replace('\n', '')
+with open(textUrl, "r", encoding='utf-8') as f:
+    text = f.read().replace('\n', '').replace('\N{SOFT HYPHEN}', '').replace('\u200b','')
     tokens = word_tokenize(text)
     tokenSent = sent_tokenize(text)
 
-wordTokens = [x for x in tokens if (x in words) and (len(x)>3)]
-# 单词变成原型
-wordRoot = [nltk.PorterStemmer().stem(word) for word in wordTokens]
-wordRoot = list(dict.fromkeys(wordRoot))
-wordRoot = [x for x in wordRoot if x not in list(set_of_common_words)]
-# 找出含有这个单词的完整句子
-sentHvWord = [[s for s in tokenSent if w in s] for w in wordRoot]
 
+
+# 剔除简单字符
+tokens = [spell.correction(stripword(x)) for x in tokens]
+wordTokens = [x for x in tokens if (x in words) and len(x) > 3]
+wordTokens = list(dict.fromkeys(wordTokens))  # 去重
+# 按单词的原型剔除常用词
+wordTokens = [x for x in wordTokens if WordNetLemmatizer().lemmatize(x) not in list(set_of_common_words)]
+# 找出含有这个单词的完整句子, 生成例句，多个空格则只保留一个空格。
+exampleSent = [[' '.join(s.split()) for s in tokenSent if w in s] for w in wordTokens]
+
+# 单词变成原型后翻译
+wordRoot = [WordNetLemmatizer().lemmatize(w) for w in wordTokens]
+wordRoot = [x for x in wordRoot if x not in list(set_of_common_words)]
 # 找出单词的含义
-pydict().meaning('brother')  # En-En
+# pydicTrans = [pydict().meaning(t) for t in wordRoot]  # En-En
+
 # EN-CN 使用 ECDICT
+dictCsvTrans = DictCsv("ecdict.csv").query_batch(wordRoot)
+
+transWord = [
+    {v for k, v in d.items() if k in ['word', 'phonetic', 'translation', 'definition']} if d is not None else None
+    for d in dictCsvTrans]
+
+# 导出至TXT文件
+output = pd.DataFrame(
+    {'word': wordRoot,
+     'example': exampleSent,
+     'translate': transWord
+     })
+
+output.to_csv(os.getcwd() + r'\anki.txt', header=False, index=False, sep='\t', mode='w')
+
+# 剔除原型重复的单词
 
 # 导出ANKI 卡片，符合ANKI特殊格式的txt文件即可
+
+#
+# pydict().meaning('ridukulus')
 
 # # 再找出高频词
 # freqs = nltk.FreqDist(wordTokens)
